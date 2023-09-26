5c85a486f47e2561082ef963b749165e2dfc86a5
SIV0027 <126695623+SIV0027@users.noreply.github.com>
2023-09-26 09:49:17 +0200

    Update main.py

--- BIA/cvik1/main.py
+++ BIA/cvik1/main.py
@@ -8,20 +8,44 @@ class Solution:
 
     def sphere(self, params):
         ret = 0
-
         for i in params:
             ret += i**2
 
         return ret
 
-    def visualize(self):
+    def ackley(self, a, b, c, params):
+        d = len(params)
+
+        #first
+        sumx = 0
+        for i in params:
+            sumx += i**2
+
+        sqrt = np.sqrt((1 / d) * sumx)
+        exp_first = np.exp(-b * sqrt)
+        first = -a * exp_first
+
+        #second
+        sumcos = 0
+        for i in params:
+            sumcos += np.cos(c * i)
+
+        second = np.exp((1 / d) * sumcos)
+
+        #summary
+        ret = first - second + a + np.exp(1)
+
+        return ret
+
+
+    def visualize(self, func):
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1, projection='3d')
 
-        x1axis = np.linspace(-5, 5, 100)
-        x2axis = np.linspace(-5, 5, 100)
+        x1axis = np.linspace(-5, 5, 500)
+        x2axis = np.linspace(-5, 5, 500)
         x1, x2 = np.meshgrid(x1axis, x2axis)
-        f1_2 = self.sphere([x1, x2])
+        f1_2 = func(20, 0.2, 2 * np.pi, [x1, x2])
 
         surf = ax.plot_surface(x1, x2, f1_2, cmap="jet")
 
@@ -34,4 +58,4 @@ class Solution:
 
 if __name__ == "__main__":
     sl = Solution("Sphere")
-    sl.visualize()
+    sl.visualize(sl.ackley)
