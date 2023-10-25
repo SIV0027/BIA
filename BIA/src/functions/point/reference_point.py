bf9167596b4b4ecae1d641ca14109f49291b7029
SIV0027 <126695623+SIV0027@users.noreply.github.com>
2023-10-25 21:04:09 +0200

    Update reference_point.py

--- BIA/src/functions/point/reference_point.py
+++ BIA/src/functions/point/reference_point.py
@@ -8,6 +8,39 @@ class Reference_point:
         self.params: List[float] = params
         self.value: float = value
 
+    def __add__(self, other):
+        other_params: List[float] = other.get_params()
+
+        params: List[float] = []
+        for i, param in self.params:
+            other_param: float = other_params[i]
+            add: float = param + other_param
+            params.append(add)
+
+        add_point = Reference_point(params, None)
+        return add_point
+
+    def __sub__(self, other):
+        other_params: List[float] = other.get_params()
+
+        params: List[float] = []
+        for i, param in self.params:
+            other_param: float = other_params[i]
+            sub: float = param - other_param
+            params.append(sub)
+
+        sub_point = Reference_point(params, None)
+        return sub_point
+    
+    def __mul__(self, other: float):
+        params: List[float] = []
+        for param in self.params:
+            mul: float = other * param
+            params.append(mul)
+
+        mul_point = Reference_point(param, None)
+        return mul_point
+
     def get_params(self) -> List[float]:
         return self.params
  
@@ -15,4 +48,4 @@ class Reference_point:
         return self.value
   
     def get_dimension(self) -> int:
-        return len(self.params)    
+        return len(self.params)
